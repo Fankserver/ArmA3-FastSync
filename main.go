b21cce7558cc67f2261757253333a9e673f49f90
nano2K <niko.bochan@gmail.com>
2014-06-27 16:21:44 +0200

    wip work

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	//"github.com/goinggo/workpool"
+	"github.com/goinggo/workpool"
 	//"io"
 	"io/ioutil"
 	"log"
@@ -30,10 +30,24 @@ type SynchFile struct {
 	Files   []DownloadFile `json:"files"`
 }
 
+type DownloadWork struct {
+	Url      string
+	Checksum string
+	WP       *workpool.WorkPool
+}
+
+func (d *DownloadWork) DoWork(workRoutine int) {
+	// check whether the file already exists
+	if _, err := os.Stat(filename); err == nil {
+		log.Printf("file already exists: %s checking checksum...")
+		process(filename)
+	}
+}
+
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	//workPool := workpool.New(runtime.NumCPU(), MAX_CONCURRENT_DOWNLOADS)
+	workPool := workpool.New(runtime.NumCPU(), MAX_CONCURRENT_DOWNLOADS)
 	bps := new(counter.Counter)
 
 	// parse flags
@@ -44,7 +58,7 @@ func main() {
 	// process input file
 	file, e := ioutil.ReadFile(*syncpath)
 	if e != nil {
-		log.Fatalf("config error: %v\n", e)
+		log.Fatalf("%v\n", e)
 		return
 	}
 
@@ -55,11 +69,25 @@ func main() {
 		return
 	}
 
-	/*go func(c *counter.Counter) {
-		for {
+	filesPending := len(sync.Files)
+	log.Printf("Files: %d", filesPending)
 
+	go func() {
+		for k := range sync.Files {
+			work := &DownloadWork{
+				Url:      sync.Files[k].Url,
+				Checksum: sync.Files[k].Checksum,
+				WP:       workPool,
+			}
+
+			err := workPool.PostWork("work_queue_routine", work)
+
+			if err != nil {
+				fmt.Printf("ERROR: %s\n", err)
+				time.Sleep(100 * time.Millisecond)
+			}
 		}
-	}(bps)*/
+	}()
 
 	for {
 		select {
